65e9520eb0f2a638662bb24a37846789d11e7864
Joeper214 <joeper.serrano@gmail.com>
2017-05-18 07:33:27 +0000

    Link Articles to Users

--- README.md
+++ README.md
@@ -52,3 +52,20 @@ Signing up users -
 Invalid signup
 
 - do an invalid sign-up and ensure that it fails
+
+
+Articles will belong to users
+
+Users will own articles
+Users - 1 side of this
+
+Articles - many
+
+1 user can have many articles
+an article can only belong to 1 user
+
+foreign key of user_id in the articles table
+
+steps:
+
+- Create the spec
--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.feature "Creating Articles" do
+  
+  before do
+    @john = User.create!(email: "john@example.com", password: "password")
+    login_as(@john)
+  end
+  
   scenario "A user creates a new article" do
     visit "/"
     
@@ -8,19 +14,23 @@ RSpec.feature "Creating Articles" do
     
     fill_in "Title", with: "Creating a blog"
     fill_in "Body", with: "Lorem Ipsum"
+    
     click_button "Create Article"
     
+    expect(Article.last.user).to eq(@john)
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
+    expect(page).to have_content("Created by: #{@john.email}")
   end
   
-  scenario "A user vails to create a new article" do
+  scenario "A user fails to create a new article" do
     visit "/"
     
     click_link "New Article"
     
     fill_in "Title", with: ""
     fill_in "Body", with: ""
+    
     click_button "Create Article"
     
     expect(page).to have_content("Article has not been created")
